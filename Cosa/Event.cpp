a1219f2610ed6407b4373c410c65b174a64a5f3b
Mikael Patel <mikael.patel@gmail.com>
2012-11-15 22:18:02 +0100

    Avoid allocating event queue with malloc.

--- Cosa/Event.cpp
+++ Cosa/Event.cpp
@@ -28,7 +28,8 @@
 
 #include "Event.h"
 
-Queue Event::queue(Event::QUEUE_MAX, sizeof(Event));
+static Event event[Event::QUEUE_MAX];
+Queue Event::queue(Event::QUEUE_MAX, sizeof(Event), event);
 
 uint8_t
 Event::push(Type type, void* source, uint16_t value)
