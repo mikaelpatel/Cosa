4cd5cf264aa2b395186a98fe7e54a7088149ad2e
Mikael Patel <mikael.patel@gmail.com>
2012-11-27 23:50:56 +0100

    Adding example code for periodic functions using the event dispatcher.

---
+++ examples/CosaBlinkTimeout/CosaBlinkTimeout.ino
@@ -0,0 +1,66 @@
+/**
+ * @file CosaBlinkTimeout.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2012, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * @section Description
+ * Cosa RGB LED blink with timeout events.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/Pins.h"
+#include "Cosa/Watchdog.h"
+
+// Use an RGB LED connected to pins(5,6,7)
+OutputPin redLedPin(5);
+OutputPin greenLedPin(6, 1);
+OutputPin blueLedPin(7);
+
+// Event handler for toggling the output pin
+void blink(Thing* it, uint8_t type, uint16_t value)
+{
+  OutputPin* pin = (OutputPin*) it;
+  pin->toggle();
+}
+
+void setup()
+{
+  // Set the blink function as the led event handler
+  redLedPin.set_event_handler(blink);
+  greenLedPin.set_event_handler(blink);
+  blueLedPin.set_event_handler(blink);
+
+  // Add the led pins to watchdog timeout periods
+  Watchdog::add(&redLedPin, 512);
+  Watchdog::add(&greenLedPin, 1024);
+  Watchdog::add(&blueLedPin, 1024);
+
+  // Start the watchdog (16 ms timeout, push timeout events)
+  Watchdog::begin(16, SLEEP_MODE_IDLE, Watchdog::push_timeout_events);
+}
+
+void loop()
+{
+  // The basic event dispatcher
+  Event event;
+  Event::queue.await(&event);
+  event.dispatch();
+}
---
+++ examples/CosaBlinkTimeout/README.md
@@ -0,0 +1,4 @@
+CosaBlinkTimeout
+====
+
+The classic LED blink demonstration written for an RGB LED using three difference time periods for the different colors. This example shows how to set an event handler for Pin objects and attach them to different Watchdog time period queues. When the time period is exceeded the Watchdog interrupt handler will push a timeout event to the time period queue (which is a Thing set; Things). The timeout event is then dispatched which will boardcast the timeout event to all the attached event handlers.
