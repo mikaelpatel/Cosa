ec5b6e905a98f342de61ae90c3cc0ad73a848907
Mikael Patel <mikael.patel@gmail.com>
2012-11-29 20:46:53 +0100

    Allowing timed state machines (timeout event in each state).
    1. Added new function and member to handle timed state machines; is_timed.
    2. Improving robustness. Check obvious runtime errors and avoiding crash.

--- Cosa/FSM.h
+++ Cosa/FSM.h
@@ -39,7 +39,8 @@ class FSM : public Thing {
 public:
   /**
    * State handler function prototype. Should return true(1) if
-   * the event was handled otherwise false(0).
+   * the event was handled otherwise false(0). This allows common
+   * and hierarchical state handlers.
    * @param[in] fsm finite state machine.
    * @param[in] type the type of event.
    * @param[in] value the event value.
@@ -49,9 +50,11 @@ public:
   
 private:
   StateHandler _state;
+  uint8_t _is_timed;
 
   /**
-   * The first level event handler. Filters timeout events.
+   * The first level event handler. Filters timeout events and
+   * adapt to state handler function prototype.
    * @param[in] it the target object.
    * @param[in] type the type of event.
    * @param[in] value the event value.
@@ -59,7 +62,7 @@ private:
   static void do_event(Thing* it, uint8_t type, uint16_t value)
   {
     FSM* fsm = (FSM*) it;
-    if (type == Event::TIMEOUT_TYPE) fsm->cancel_timer();
+    if (!fsm->_is_timed) fsm->cancel_timer();
     fsm->_state(fsm, type, value);
   }
 
@@ -67,8 +70,13 @@ public:
   /**
    * Construct state machine with given initial state.
    * @param{in] init initial state handler.
+   * @param{in] is_timed state machine needs a timeout in all states.
    */
-  FSM(StateHandler init = 0) : Thing(do_event), _state(init) { }
+  FSM(StateHandler init, uint8_t is_timed = 0) : 
+    Thing(do_event), 
+    _state(init),
+    _is_timed(is_timed)
+  {}
   
   /**
    * Set new state handler for next event.
@@ -76,7 +84,18 @@ public:
    */
   void set_state(StateHandler fn) 
   {
+    if (fn == 0) return;
     _state = fn;
+  }
+  
+  /**
+   * Set timed status; true(1) to receive timeout events in all 
+   * states.
+   * @param[in] flag for timed status.
+   */
+  void set_timed(uint8_t flag)
+  {
+    _is_timed == flag;
   }
   
   /**
@@ -104,6 +123,7 @@ public:
    */
   void begin()
   {
+    if (_state == 0) return;
     send(Event::FSM_BEGIN_TYPE);
   }
   
@@ -125,7 +145,8 @@ public:
   }
 
   /**
-   * Cancel a timer request. 
+   * Cancel a timer request. This is performed by default on a
+   * timeout event.
    */
   void cancel_timer()
   {
