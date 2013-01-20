b965f2af923859fd18afb89fd291616533c04156
Mikael Patel <mikael.patel@gmail.com>
2013-01-20 11:08:23 +0100

    Started port to Arduino Mega 2560. Adding board and pin definitions.

--- Cosa/Board.hh
+++ Cosa/Board.hh
@@ -30,6 +30,10 @@
 #ifndef __COSA_BOARD_HH__
 #define __COSA_BOARD_HH__
 
+#if defined(__AVR_ATmega8__)   || \
+    defined(__AVR_ATmega168__) || \
+    defined(__AVR_ATmega328P__)
+
 class Board {
   friend class Pin;
 private:
@@ -45,7 +49,9 @@ private:
    */
   static volatile uint8_t* SFR(uint8_t pin) 
   { 
-    return (pin < 8 ? &PIND : (pin < 14 ? &PINB : &PINC));
+    return (pin < 8 ? &PIND : 
+	    pin < 14 ? &PINB : 
+	    &PINC);
   }
 
   /**
@@ -56,7 +62,7 @@ private:
    */
   static const uint8_t BIT(uint8_t pin)
   {
-    return (pin < 8 ? pin : (pin < 14 ? pin - 8 : (pin - 14)));
+    return (pin < 8 ? pin : pin < 14 ? pin - 8 : pin - 14);
   }
   
 public:
@@ -78,7 +84,7 @@ public:
     D11,
     D12,
     D13,
-    LED = 13
+    LED = D13
   };
 
   /**
@@ -97,7 +103,158 @@ public:
   };
 
   /**
-   * PWM pin symbols; sub-set of digital pins to allow compile time checking
+   * PWM pin symbols; sub-set of digital pins to allow compile 
+   * time checking
+   */
+  enum PWMPin {
+#if !defined(__AVR_ATmega8__)
+    PWM0 = 3,
+    PWM1 = 5,
+    PWM2 = 6,
+#endif
+    PWM3 = 9,
+    PWM4 = 10,
+    PWM5 = 11
+  };
+
+  /**
+   * External interrupt pin symbols; sub-set of digital pins 
+   * to allow compile time checking.
+   */
+  enum InterruptPin {
+    EXT0 = 2,
+    EXT1 = 3
+  };
+};
+
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+
+class Board {
+  friend class Pin;
+private:
+  /**
+   * Do not allow instances. This is a static singleton.
+   */
+  Board() {}
+
+  /**
+   * Return Special Function Register for given Arduino pin number.
+   * @param[in] pin number.
+   * @return special register pointer.
+   */
+  static volatile uint8_t* SFR(uint8_t pin) 
+  { 
+    return (pin < 8  ? &PINE : 
+	    pin < 16 ? &PINH : 
+	    pin < 24 ? &PINB : 
+	    pin < 32 ? &PINA : 
+	    pin < 40 ? &PINC : 
+	    pin < 48 ? &PIND : 
+	    pin < 56 ? &PINL : 
+	    pin < 64 ? &PINF : 
+	    pin < 72 ? &PINK : 
+	    pin < 80 ? &PINJ : 
+	    &PING);
+  }
+
+  /**
+   * Return bit position for given Arduino pin number in Special
+   * Function Register.
+   * @param[in] pin number.
+   * @return pin bit position.
+   */
+  static const uint8_t BIT(uint8_t pin)
+  {
+    return (pin & 0x7);
+  }
+  
+public:
+  /**
+   * Digital pin symbols
+   */
+  enum DigitalPin {
+    D0 = 0,
+    D1 = 1,
+    D2 = 4,
+    D3 = 5,
+    D4 = 93,
+    D5 = 3,
+    D6 = 11,
+    D7 = 12,
+    D8 = 13,
+    D9 = 14,
+    D10 = 20,
+    D11 = 21,
+    D12 = 22,
+    D13 = 23,
+    D14 = 81,
+    D15 = 80,
+    D16 = 9,
+    D17 = 8,
+    D18 = 51,
+    D19 = 50,
+    D20 = 41,
+    D21 = 40,
+    D22 = 24,
+    D23 = 25,
+    D24 = 26,
+    D25 = 27,
+    D26 = 28,
+    D27 = 29,
+    D28 = 30,
+    D29 = 31,
+    D30 = 39,
+    D31 = 38,
+    D32 = 37,
+    D33 = 36,
+    D34 = 35,
+    D35 = 34,
+    D36 = 33,
+    D37 = 32,
+    D38 = 47,
+    D39 = 82,
+    D40 = 81,
+    D41 = 80,
+    D42 = 55,
+    D43 = 54,
+    D44 = 53,
+    D45 = 52,
+    D46 = 51,
+    D47 = 50,
+    D48 = 49,
+    D49 = 48,
+    D50 = 19,
+    D51 = 18,
+    D52 = 17,
+    D53 = 16,
+    LED = D13
+  };
+
+  /**
+   * Analog pin symbols
+   */
+  enum AnalogPin {
+    A0 = 56,
+    A1 = 57,
+    A2 = 58,
+    A3 = 59,
+    A4 = 60,
+    A5 = 61,
+    A6 = 62,
+    A7 = 63,
+    A8 = 64,
+    A9 = 65,
+    A10 = 66,
+    A11 = 67,
+    A12 = 68,
+    A13 = 69,
+    A14 = 70,
+    A15 = 71
+  };
+
+  /**
+   * PWM pin symbols; sub-set of digital pins to allow compile 
+   * time checking
    */
   enum PWMPin {
     PWM0 = 3,
@@ -109,8 +266,8 @@ public:
   };
 
   /**
-   * External interrupt pin symbols; sub-set of digital pins to allow compile 
-   * time checking.
+   * External interrupt pin symbols; sub-set of digital pins 
+   * to allow compile time checking.
    */
   enum InterruptPin {
     EXT0 = 2,
@@ -118,5 +275,6 @@ public:
   };
 };
 
+#endif
 #endif
 
