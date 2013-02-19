062e7e240d8ce3e647b00b82cc979b13a3a69d31
Mikael Patel <mikael.patel@gmail.com>
2013-02-19 22:33:47 +0100

    Adding example for the servo motor class.

---
+++ examples/Demo/CosaServo/CosaServo.ino
@@ -0,0 +1,61 @@
+/**
+ * @file CosaServo.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * @section Description
+ * Demonstration of the Servo control class.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/Types.h"
+#include "Cosa/Trace.hh"
+#include "Cosa/Servo.hh"
+#include "Cosa/Watchdog.hh"
+#include "Cosa/IOStream/Driver/UART.hh"
+
+Servo servo(0, Board::D9);
+Servo door(1, Board::D8);
+
+void setup()
+{
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaServo: started"));
+  Servo::begin();
+  Watchdog::begin();
+}
+
+void loop()
+{
+  int angle;
+  for (uint8_t inc = 5; inc < 20; inc += 5) {
+    door.set_pos(0);
+    for (angle = 0; angle < 180; angle += inc) {
+      servo.set_pos(angle);
+      Watchdog::delay(64);
+    }
+    door.set_pos(180);
+    for (; angle > 0; angle -= inc) {
+      servo.set_pos(angle);
+      Watchdog::delay(64);
+    }
+  }
+}
---
+++ examples/Demo/CosaServo/README.md
@@ -0,0 +1,12 @@
+CosaServo
+=============
+
+Demonstration of servo motor control class; Servo. 
+
+
+Circuit
+-------
+Connect two servo motors to pins D8 and D9.
+
+
+
