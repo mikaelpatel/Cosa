22e13666830eef13071573ddb388d1372ef2fd73
Mikael Patel <mikael.patel@gmail.com>
2013-04-06 00:56:39 +0200

    Added header to configuration register sandbox file.

--- examples/Sandbox/CosaBitfields/config.h
+++ examples/Sandbox/CosaBitfields/config.h
@@ -1,3 +1,32 @@
+/**
+ * @file config.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * @section Description
+ * Evaluate different methods of bit-field access using macro, enum,
+ * shift and mask, and bit-fields struct definitions.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
 #ifndef __CONFIG_H__
 #define __CONGIG_H__
 
