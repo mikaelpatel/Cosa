6b97c59f1769cd7aea6724a41d1511a81a8f8c3d
Mikael Patel <mikael.patel@gmail.com>
2013-04-22 21:04:49 +0200

    Fixed include of Board.hh to allow ATtiny to ignore print/IOStream/trace.

--- examples/OWI/CosaDS18B20/CosaDS18B20.ino
+++ examples/OWI/CosaDS18B20/CosaDS18B20.ino
@@ -28,6 +28,7 @@
  * This file is part of the Arduino Che Cosa project.
  */
 
+#include "Cosa/Board.hh"
 #include "Cosa/OWI/Driver/DS18B20.hh"
 #include "Cosa/Trace.hh"
 #include "Cosa/IOStream/Driver/UART.hh"
