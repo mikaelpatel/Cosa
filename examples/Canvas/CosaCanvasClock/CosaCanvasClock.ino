38d47cadad272f809fd70e2acaa31918abdeec85
Mikael Patel <mikael.patel@gmail.com>
2013-05-06 12:25:22 +0200

    Removing display flicker.

--- examples/Canvas/CosaCanvasClock/CosaCanvasClock.ino
+++ examples/Canvas/CosaCanvasClock/CosaCanvasClock.ino
@@ -38,43 +38,59 @@
 #include "Cosa/Canvas/Font/Segment32x50.hh"
 
 ST7735R tft;
+const Canvas::color16_t BACKGROUND = tft.shade(Canvas::RED, 25);
+const Canvas::color16_t TEXT_COLOR = tft.shade(Canvas::RED, 75);
+uint8_t min = 30;
+uint8_t sec = 00;
 
 void setup()
 {
   // Start the watchdog for low power sleep
-  Watchdog::begin();
+  Watchdog::begin(1024, SLEEP_MODE_IDLE, Watchdog::push_watchdog_event);
 
   // Initiate the display and set orientation and color scheme
   tft.begin();
   tft.set_canvas_color(Canvas::BLACK);
   tft.set_orientation(Canvas::LANDSCAPE);
   tft.set_text_font(&segment32x50);
-  tft.set_text_color(tft.shade(Canvas::RED, 75));
+  tft.set_text_color(TEXT_COLOR);
   tft.fill_screen();
-}
-
-void loop()
-{
-  SLEEP(1);
-  static uint8_t min = 30;
-  static uint8_t sec = 00;
 
   // Draw a filled boundary box using the round rectangle
-  Canvas::color16_t saved = tft.set_pen_color(tft.shade(Canvas::RED, 25));
+  Canvas::color16_t saved = tft.set_pen_color(BACKGROUND);
   tft.fill_roundrect(0, 20, tft.WIDTH - 1, 70, 12);
-  tft.set_pen_color(tft.get_text_color());
+  tft.set_pen_color(TEXT_COLOR);
   tft.draw_roundrect(0, 20, tft.WIDTH - 1, 70, 12);
   tft.set_pen_color(saved);
 
-  // Draw the current counter value
+  // Draw initial time
   tft.set_cursor(8, 30);
   tft.draw_char('0' + min/10);
   tft.draw_char('0' + min%10);
   tft.move_cursor(-12, 0);
-  tft.draw_char('9' + 1);
+  tft.draw_char(':');
   tft.move_cursor(-8, 0);
   tft.draw_char('0' + sec/10);
   tft.draw_char('0' + sec%10);
+}
+
+void draw_digit(uint8_t pos, uint8_t digit)
+{
+  uint8_t x, y;
+  uint8_t adjust = (pos < 2 ? 0 : segment32x50.WIDTH - 17);
+  tft.set_cursor(8 + (pos * segment32x50.WIDTH) + adjust, 30);
+  Canvas::color16_t saved = tft.set_pen_color(BACKGROUND);
+  tft.get_cursor(x, y);
+  tft.fill_rect(x, y, segment32x50.WIDTH, segment32x50.HEIGHT);
+  tft.set_pen_color(saved);
+  tft.draw_char('0' + digit);
+}
+
+void loop()
+{
+  // Receive a watchdog event every second
+  Event event;
+  Event::queue.await(&event);
 
   // Decrement counter
   if (sec == 0) {
@@ -82,6 +98,16 @@ void loop()
       sec = 59;
       min -= 1;
     }
-  } else
+  } else {
     sec -= 1;
+  }
+
+  // Draw the digits that are updated (to avoid flicker)
+  draw_digit(3, sec%10);
+  if ((sec%10) != 9) return;
+  draw_digit(2, sec/10);
+  if ((sec/10) != 5) return;
+  draw_digit(1, min%10);
+  if ((min%10) != 9) return;
+  draw_digit(0, min/10);
 }
