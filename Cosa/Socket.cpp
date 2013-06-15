5388ac0c6da99903541aaa43c24bf06f4e0b9437
Mikael Patel <mikael.patel@gmail.com>
2013-06-16 00:00:12 +0200

    Adding support for socket (connectionless and connection-oriented) communication.

---
+++ Cosa/Socket.cpp
@@ -0,0 +1,96 @@
+/**
+ * @file Cosa/Socket.cpp
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/Socket.hh"
+
+IOStream& operator<<(IOStream& outs, const Socket::addr_t& src)
+{
+  univ32_t addr;
+  addr.as_uint32 = src.addr;
+  outs << addr.as_uint8[3] << '.'
+       << addr.as_uint8[2] << '.'
+       << addr.as_uint8[1] << '.'
+       << addr.as_uint8[0] << ':' 
+       << src.port;
+  return (outs);
+}
+
+int
+Socket::Device::attach(Socket* s)
+{
+  int res = -1;
+  for (uint8_t i = 0; i < SOCKET_MAX; i++) {
+    if (m_socket[i] == s) return (i);
+    if (m_socket[i] == 0 && res < 0) res = i;
+  }
+  if (res != -1) m_socket[res] = s;
+  return (res);
+}
+
+void
+Socket::Device::detach(Socket* s)
+{
+  for (uint8_t i = 0; i < SOCKET_MAX; i++) {
+    if (m_socket[i] != s) continue;
+    m_socket[i] = 0;
+    return;
+  }
+}
+
+Socket*
+Socket::Device::lookup(uint16_t port)
+{
+  for (uint8_t i = 0; i < SOCKET_MAX; i++) {
+    Socket* s = m_socket[i];
+    if (s == 0) continue;
+    if (s->m_port != port) continue;
+    return (s);
+  }
+  return (0);
+}
+
+void 
+Socket::Device::set_connected(Client* client)
+{
+  client->m_state = Client::CONNECTED_STATE;
+}
+
+void 
+Socket::Device::set_disconnected(Client* client)
+{
+  client->m_state = Client::DISCONNECTED_STATE;
+}
+
+void 
+Socket::Device::set_port(Client* client, uint16_t port)
+{
+  client->m_port = port;
+}
+
+void 
+Socket::Device::get_dest_address(Client* client, Socket::addr_t& dest)
+{
+  dest = client->m_dest;
+}
