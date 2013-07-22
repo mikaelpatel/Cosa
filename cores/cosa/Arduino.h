6e878f851478fa93cf627ede47a302950325fd6a
Mikael Patel <mikael.patel@gmail.com>
2013-07-22 13:40:58 +0200

    Refactoring directory structure to a hardware core package.

---
+++ cores/cosa/Arduino.h
@@ -0,0 +1,29 @@
+/**
+ * @file Arduino.h
+ * @version 1.0
+ *
+ * @section Description
+ *
+ *   The Bellman himself they all praised to the skies--
+ *   Such a carriage, such ease and such grace!
+ *   Such solemnity, too! One could see he was wise,
+ *   The moment one looked in his face!
+ *
+ *   He had bought a large map representing the sea,
+ *   Without the least vestige of land:
+ *   And the crew were much pleased when they found it to be
+ *   A map they could all understand.
+ *
+ *   "What's the good of Mercator's North Poles and Equators,
+ *   Tropics, Zones, and Meridian Lines?"
+ *   So the Bellman would cry: and the crew would reply
+ *   "They are merely conventional signs!
+ *
+ *   "Other maps are such shapes, with their islands and capes!
+ *   But we've got our brave Captain to thank:
+ *   (So the crew would protest) "that he's bought us the best--
+ *   A perfect and absolute blank!" 
+ *
+ *                       Lewis Carroll, The Hunting of the Snark
+ */
+
