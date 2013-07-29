d8b92795a3bd90b98b1a5c7219de2a4f030e024f
Mikael Patel <mikael.patel@gmail.com>
2013-07-29 11:45:10 +0200

    Adding support for autokey cipher; Vigenere.

---
+++ cores/cosa/Cosa/Crypto/Vigenere.hh
@@ -0,0 +1,113 @@
+/**
+ * @file Cosa/Crypto/Vigenere.hh
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#ifndef __COSA_CRYPTO_VIGENERE_HH__
+#define __COSA_CRYPTO_VIGENERE_HH__
+
+#include "Cosa/Types.h"
+
+/**
+ * Vigenere auto-key cipher.
+ * @section See Also
+ * http://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+ */
+template<uint8_t N> 
+class Vigenere {
+private:
+  char m_key[N];
+  uint8_t m_nr;
+  uint8_t m_max;
+  uint8_t m_pos;
+
+public:
+  /**
+   * Construct Vigenere crypto with key generated from given seed.
+   * An autokey is generated for when n is less than N (max key length).
+   * @param[in] seed for random key generation.
+   * @param[in] n length of random key.
+   */
+  Vigenere(uint32_t seed, uint8_t n = N) 
+  { 
+    if (n > N) n = N;
+    srandom(seed);
+    for (uint8_t i = 0; i < n; i++) 
+      m_key[i] = random();
+    m_nr = 0;
+    m_max = n;
+    m_pos = n;
+  }
+
+  /**
+   * Construct Vigenere crypto with given password. An autokey is
+   * generated for when password length is less than N (max key length). 
+   * @param[in] password.
+   */
+  Vigenere(char* password)
+  { 
+    uint8_t i;
+    for (i = 0; i < N && password[i]; i++) 
+      m_key[i] = password[i];
+    m_nr = 0;
+    m_max = i;
+    m_pos = i;
+  }
+
+  /**
+   * Restart the crypto. Generate a new auto-key if needed.
+   */
+  void restart()
+  {
+    m_max = m_pos;
+    m_nr = 0;
+  }
+
+  /**
+   * Encode the given character.
+   * @param[in] c character to encode.
+   * @return encoded character.
+   */
+  char encode(char c) 
+  {
+    char res = c + m_key[m_nr++];
+    if (m_max != N) m_key[m_max++] = c;
+    if (m_nr == N) m_nr = 0;
+    return (res);
+  }
+
+  /**
+   * Decode the given character.
+   * @param[in] c character to decode.
+   * @return decoded character.
+   */
+  char decode(char c)
+  {
+    char res = c - m_key[m_nr++];
+    if (m_max != N) m_key[m_max++] = res;
+    if (m_nr == N) m_nr = 0;
+    return (res);
+  }
+};
+
+#endif
---
+++ examples/Sandbox/CosaVigenere/CosaVigenere.ino
@@ -0,0 +1,91 @@
+/**
+ * @file CosaVigenere.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * @section Description
+ * Cosa demonstration of Vigenere cipher.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/Crypto/Vigenere.hh"
+#include "Cosa/Trace.hh"
+#include "Cosa/IOStream/Driver/UART.hh"
+#include "Cosa/RTC.hh"
+
+char msg[] PROGMEM = 
+"The Vigenere cipher is a method of encrypting alphabetic text by\n"
+"using a series of different Caesar ciphers based on the letters of\n" 
+"a keyword. It is a simple form of polyalphabetic substitution.\n"
+"\n"
+"The Vigenere cipher has been reinvented many times. The method was\n"
+"originally described by Giovan Battista Bellaso in his 1553 book \n"
+"La cifra del. Sig. Giovan Battista Bellaso; however, the scheme was\n"
+"later misattributed to Blaise de Vigenere in the 19th century,\n" 
+"and is now widely known as the Vigenere cipher.\n" 
+"\n" 
+"Though the cipher is easy to understand and implement,\n" 
+"for three centuries it resisted all attempts to break it;\n" 
+"this earned it the description le chiffre indechiffrable\n" 
+"(French for 'the indecipherable cipher'). Many people have\n" 
+"tried to implement encryption schemes that are essentially\n" 
+"Vigenere ciphers. Friedrich Kasiski was the first to publish\n" 
+"a general method of deciphering a Vigenere cipher.";
+
+void setup()
+{
+  RTC::begin();
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaVigenere: started"));
+
+  // Test#1: Encode the message and measure processing time
+  Vigenere<32> sender("QUEENLY");
+  TRACE(sizeof(sender));
+  char c;
+  const char* s = msg;
+  uint32_t start = RTC::micros();
+  while ((c = pgm_read_byte(s++)) != 0)
+    c = sender.encode(c);
+  uint32_t us = RTC::micros() - start;
+  uint16_t len = strlen_P(msg);
+  trace << len << PSTR(" bytes, ")
+	<< us << PSTR(" us (")
+	<< (us * 1000) / len << PSTR(" ns/byte)")
+	<< endl;
+
+  // Test#2: Running the message through the encoder
+  trace << PSTR("ENCODED MESSAGE") << endl;
+  sender.restart();
+  s = msg;
+  while ((c = pgm_read_byte(s++)) != 0)
+    trace << sender.encode(c);
+
+  // Test#3: Running the message through the encoder/decoder
+  trace << PSTR("DECODED MESSAGE") << endl;
+  Vigenere<32> receiver("QUEENLY");
+  sender.restart();
+  s = msg;
+  while ((c = pgm_read_byte(s++)) != 0)
+    trace << receiver.decode(sender.encode(c));
+}
+
+
+
