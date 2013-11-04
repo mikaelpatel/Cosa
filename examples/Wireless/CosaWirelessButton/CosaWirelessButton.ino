b59edea18485f370a788ab769be1f7decefe1088
Mikael Patel <mikael.patel@gmail.com>
2013-11-04 01:26:38 +0100

    Testing CC1101 device driver build.

--- examples/Wireless/CosaWirelessButton/CosaWirelessButton.ino
+++ examples/Wireless/CosaWirelessButton/CosaWirelessButton.ino
@@ -35,20 +35,20 @@
 #include "Cosa/RTC.hh"
 
 // Select Wireless device driver (network = 0xC05A, device = 0x02)
-// #include "Cosa/Wireless/Driver/CC1101.hh"
-// CC1101 rf(0xC05A, 0x02);
+#include "Cosa/Wireless/Driver/CC1101.hh"
+CC1101 rf(0xC05A, 0x02);
 
 // #include "Cosa/Wireless/Driver/NRF24L01P.hh"
 // NRF24L01P rf(0xC05A, 0x02);
 
-#include "Cosa/Wireless/Driver/VWI.hh"
-#include "Cosa/Wireless/Driver/VWI/Codec/VirtualWireCodec.hh"
-VirtualWireCodec codec;
-#if defined(__ARDUINO_TINYX5__)
-VWI rf(0xC05A, 0x03, 4000, Board::D1, Board::D0, &codec);
-#else
-VWI rf(0xC05A, 0x02, 4000, Board::D7, Board::D8, &codec);
-#endif
+// #include "Cosa/Wireless/Driver/VWI.hh"
+// #include "Cosa/Wireless/Driver/VWI/Codec/VirtualWireCodec.hh"
+// VirtualWireCodec codec;
+// #if defined(__ARDUINO_TINYX5__)
+// VWI rf(0xC05A, 0x03, 4000, Board::D1, Board::D0, &codec);
+// #else
+// VWI rf(0xC05A, 0x02, 4000, Board::D7, Board::D8, &codec);
+// #endif
 
 // Connect button between ground and pin TinyX4 EXT0/D10, TinyX5
 // EXT0/D2, Mega EXT2/D29 and others to Arduino EXT1 which is
