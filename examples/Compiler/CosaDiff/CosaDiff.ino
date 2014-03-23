58c6516fbf23ed5fa74d0a5d5c4dd42ac8a07205
Mikael Patel <mikael.patel@gmail.com>
2014-03-23 16:26:10 +0100

    Creating a folder for compiler testing sketches. New unsigned diff sketch.

---
+++ examples/Compiler/CosaDiff/CosaDiff.ino
@@ -0,0 +1,72 @@
+/**
+ * @file CosaDiff.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2014, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ * @section Description
+ * Testing code generation of arithmetric and comparison of unsigned 
+ * numbers. Force wrap-around uint32_t and show how this workds.
+ *
+ * The conclusion is that difference should be signed and comparison
+ * operators should be avoided. Instead compare with difference to 
+ * handle wrap-around correctly.
+ *   uint32_t t0, t1;
+ *   int32_t diff = t1 - t0;
+ *   bool wrong = t1 < t0; 
+ *   bool correct = diff < 0;
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/Trace.hh"
+#include "Cosa/IOStream/Driver/UART.hh"
+
+void setup()
+{
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaDiff: started"));
+}
+
+void loop()
+{
+  iter(0xfffffff0UL, 0xfffffff4UL, 1, 1);
+  iter(0xfffffff0UL, 0xfffffff0UL, 0, 1);
+  iter(0xfffffff0UL, 0xfffffff4UL, 1, 0);
+  ASSERT(true == false);
+}
+
+void iter(uint32_t t0, uint32_t t1, uint8_t i0, uint8_t i1)
+{
+  for (int i = 0; i < 32; i++) {
+    int32_t diff = t1 - t0;
+    bool wrong = t1 < t0;
+    bool correct = diff < 0;
+    trace << i 
+	  << PSTR(":t0=") << t0
+	  << PSTR(",t1=") << t1
+	  << PSTR(",diff=") << diff
+	  << PSTR(",wrong=") << wrong
+	  << PSTR(",correct=") << correct
+	  << endl;
+    t0 += i0;
+    t1 += i1;
+  }
+  trace << endl;
+}
