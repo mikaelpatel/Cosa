cbba0d217419a55ee75c60c5174e3666f69dace0
Mikael Patel <mikael.patel@gmail.com>
2014-04-10 23:52:21 +0200

    Cleanup example sketches; removing README.md, moved info to sketch intro.

--- examples/Drivers/CosaEEPROM/CosaEEPROM.ino
+++ examples/Drivers/CosaEEPROM/CosaEEPROM.ino
@@ -3,7 +3,7 @@
  * @version 1.0
  *
  * @section License
- * Copyright (C) 2013, Mikael Patel
+ * Copyright (C) 2013-2014, Mikael Patel
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,6 +22,9 @@
  *
  * @section Description
  * Demo of the default EEPROM device driver.
+ *
+ * @section Circuit
+ * Uses the MCU internal EEPROM and Analog Pin A0 for samples.
  *
  * This file is part of the Arduino Che Cosa project.
  */
