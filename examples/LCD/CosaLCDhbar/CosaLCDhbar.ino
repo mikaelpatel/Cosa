5d11f94f23679206d49a6bbb91401240e489516d
Mikael Patel <mikael.patel@gmail.com>
2014-05-07 22:19:46 +0200

    Testing ERM1602-5 LCD adapter.

--- examples/LCD/CosaLCDhbar/CosaLCDhbar.ino
+++ examples/LCD/CosaLCDhbar/CosaLCDhbar.ino
@@ -58,8 +58,8 @@
 // HD44780::SR4W port;
 // HD44780::MJKDZ port;
 // HD44780::GYIICLCD port;
-HD44780::DFRobot port;
-// HD44780::ERM1602_5 port;
+// HD44780::DFRobot port;
+HD44780::ERM1602_5 port;
 HD44780 lcd(&port);
 IOStream cout(&lcd);
 #if defined(BOARD_ATTINY)
--- examples/LCD/CosaLCDvbar/CosaLCDvbar.ino
+++ examples/LCD/CosaLCDvbar/CosaLCDvbar.ino
@@ -52,14 +52,14 @@
 #include "Cosa/LCD/Driver/HD44780.hh"
 
 // LCD and communication port
-HD44780::Port4b port;
+// HD44780::Port4b port;
 // HD44780::SR3W port;
 // HD44780::SR3WSPI port;
 // HD44780::SR4W port;
 // HD44780::MJKDZ port;
 // HD44780::GYIICLCD port;
 // HD44780::DFRobot port;
-// HD44780::ERM1602_5 port;
+HD44780::ERM1602_5 port;
 HD44780 lcd(&port);
 IOStream cout(&lcd);
 
