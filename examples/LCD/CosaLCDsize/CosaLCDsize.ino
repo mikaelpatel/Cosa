006d98dcc0ce472641c41b3b1f01b0070051cc88
Mikael Patel <mikael.patel@gmail.com>
2014-05-27 17:14:39 +0200

    Rebuild all and collect statistics.

--- examples/LCD/CosaLCDsize/CosaLCDsize.ino
+++ examples/LCD/CosaLCDsize/CosaLCDsize.ino
@@ -27,28 +27,31 @@
 
 #include "Cosa/Watchdog.hh"
 
+// AVR Tools: 3.4.3, GCC: 4.3.1
 // Binary sketch size:  MEGA/STD*/TINY bytes
 // =========================================
-// Baseline:		1152/1034/1186 bytes
-// HD44780::Port4b:	3566/3418/3502 bytes
-// HD44780::SR3W:	3760/3612/3696 bytes
-// HD44780::SR3WSPI:	4330/4116/4092 bytes
-// HD44780::SR4W:	3642/3498/3588 bytes
-// HD44780::MJKDZ:	5110/5054/5722 bytes
-// HD44780::GYIICLCD:	5110/5054/5722 bytes
-// HD44780::DFRobot:	5084/5028/5696 bytes
-// HD44780::ERM1602_5:	3924/3790/3870 bytes
-// PCD8544: 		4272/4144/4088 bytes
-// ST7565:  		4442/4326/4258 bytes
-// VLCD:		4054/3996/4644 bytes
+// Baseline:		 954/ 840/ 994 bytes
+// HD44780::Port4b:	3780/3638/3726 bytes
+// HD44780::SR3W:	3692/3544/3628 bytes
+// HD44780::SR3WSPI:	4240/4026/3964 bytes
+// HD44780::SR4W:	3586/3442/3532 bytes
+// HD44780::MJKDZ:	5058/5002/5652 bytes
+// HD44780::GYIICLCD:	5058/5002/5652 bytes
+// HD44780::DFRobot:	5032/4976/5626 bytes
+// HD44780::ERM1602_5:	3866/3732/3812 bytes
+// PCD8544::Serial3W:	4660/4442/4386 bytes
+// PCD8544::SPI3W:	5198/4906/4744 bytes
+// ST7565::Serial3W:	4834/4628/4562 bytes
+// ST7565::SPI3W:	5374/5092/4920 bytes
+// VLCD:		3958/3904/4520 bytes
 // =========================================
 // The baseline is the Watchdog and Arduino
 // init code. Move comment prefix to compile 
 // different LCD drivers and compare size.
 // =========================================
 
-// #include "Cosa/LCD/Driver/HD44780.hh"
-// HD44780::Port4b port;
+#include "Cosa/LCD/Driver/HD44780.hh"
+HD44780::Port4b port;
 // HD44780::SR3W port;
 // HD44780::SR3WSPI port;
 // HD44780::SR4W port;
@@ -56,18 +59,20 @@
 // HD44780::GYIICLCD port;
 // HD44780::DFRobot port;
 // HD44780::ERM1602_5 port;
-// HD44780 lcd(&port);
+HD44780 lcd(&port);
 
 // #include "Cosa/LCD/Driver/PCD8544.hh"
-// PCD8544::Serial3W port;
-// PCD8544::SPI3W port;
-// PCD8544 lcd;
+// LCD::Serial3W port;
+// LCD::SPI3W port;
+// PCD8544 lcd(&port);
 
 // #include "Cosa/LCD/Driver/ST7565.hh"
-// ST7565 lcd;
+// LCD::Serial3W port;
+// LCD::SPI3W port;
+// ST7565 lcd(&port);
 
-#include "Cosa/LCD/Driver/VLCD.hh"
-VLCD lcd;
+// #include "Cosa/LCD/Driver/VLCD.hh"
+// VLCD lcd;
 
 void setup()
 {
