26acc05b8bab9b3450c4b2cc77eba2d2b901d5ce
Mikael Patel <mikael.patel@gmail.com>
2014-07-01 23:00:39 +0200

    Porting Arduino-Makefile to Cosa.
    1. Scaling down the Arduino-Makefile to support the Cosa build.
    2. Pre-configuration for Cosa sketches.
    3. Pre-configuration for miniterm.py as monitor.

---
+++ build/Cosa.mk
@@ -0,0 +1,30 @@
+# @file Cosa.mk
+# @version 1.0
+#
+# @section License
+# Copyright (C) 2014, Mikael Patel
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# @section Description
+# Arduino-Makefile configuration for Cosa. 
+
+# PLEASE CHANGE THE ARDUINO INSTALLATION PATH
+ARDUINO_DIR = $(HOME)/opt/arduino-1.0.5
+
+ARDMK_DIR = $(COSA_DIR)/build/Arduino-Makefile
+ARDUINO_CORE_PATH = $(COSA_DIR)/cores/cosa
+ARDUINO_VAR_PATH = $(COSA_DIR)/variants
+BOARDS_TXT = $(COSA_DIR)/boards.txt
+
+MONITOR_CMD = miniterm.py -q 
+
+include $(ARDMK_DIR)/Arduino.mk
