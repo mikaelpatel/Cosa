75834369cb0ebcd52bce99294817c6e9b09fdd49
jediunix <jediunix@users.noreply.github.com>
2014-12-07 09:49:40 -0600

    Improve Makefile checking of data/eeprom sizes
    Add maximum_eeprom_size for all boards.  Add test cases for data and
    eeprom size.  Fix some problems with makefile.

---
+++ examples/Build/CosaSizeEEPROM/CosaSizeEEPROM.ino
@@ -0,0 +1,56 @@
+/**
+ * @file CosaSizeEEPROM.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013-2014, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * @section Description
+ * Test the limits of EEPROM size against the build system and boards.txt.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#define EEPROM_SIZE  512
+//#define EEPROM_SIZE  513
+//#define EEPROM_SIZE 1024
+//#define EEPROM_SIZE 1025
+//#define EEPROM_SIZE 2048
+//#define EEPROM_SIZE 2049
+
+#include "Cosa/Trace.hh"
+#include "Cosa/IOStream/Driver/UART.hh"
+#include "Cosa/EEPROM.hh"
+
+// EEPROM access object
+EEPROM eeprom;
+
+// Simple blob in EEPROM
+uint8_t blob[EEPROM_SIZE] EEMEM = {0};
+
+void setup()
+{
+  // Use serial as output stream
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaSizeEEPROM: started"));
+
+  ASSERT(eeprom.write(&blob[0], (uint8_t) 0xff) == sizeof(uint8_t));
+}
+
+void loop()
+{
+  trace << PSTR("running") << endl;
+
+  // Take a nap
+  sleep(5);
+}
