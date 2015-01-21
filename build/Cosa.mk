227384a4a54ac2008544113475410cb9df2ce9fc
Mikael Patel <mikael.patel@gmail.com>
2015-01-21 23:21:18 +0100

    Update supported build versions.

--- build/Cosa.mk
+++ build/Cosa.mk
@@ -2,7 +2,7 @@
 # @version 1.0
 #
 # @section License
-# Copyright (C) 2014, Mikael Patel
+# Copyright (C) 2014-2015, Mikael Patel
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
