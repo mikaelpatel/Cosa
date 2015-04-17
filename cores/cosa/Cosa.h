d911dda4c897b37ee5ffe788be358458dc059f7f
jeditekunum <jeditekunum@gmail.com>
2015-04-17 09:43:47 -0500

    Cosa.h - per-application customization for Cosa
    This commit introduces Cosa.h for per-application customization of Cosa and implements the first customizable value for maximum Event queue length.
    
    Per-application customization is accomplished by copying cores/cosa/Cosa.h to the application directory and inserting relevant defines.

---
+++ cores/cosa/Cosa.h
@@ -0,0 +1,37 @@
+/**
+ * @file Cosa.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Default for per-application customization of Cosa.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#ifndef COSA_H
+#define COSA_H
+
+/* This file is intentionally empty as it is the default customization
+ * if the application does not provide one.
+ *
+ * To customize Cosa, copy this file into the application directory
+ * and place defines in it.
+ *
+ * The Cosa standard naming convention for customization defines
+ * is COSA_class_parameter. For example, COSA_EVENT_QUEUE_MAX.
+ */
+
+#endif
