e49025cf4226f6fd896165614b456364b23cf42d
Mikael Patel <mikael.patel@gmail.com>
2015-04-19 13:43:30 +0200

    Use MEASURE macro when possible.

--- examples/Canvas/CosaCanvasDemo/CosaCanvasDemo.ino
+++ examples/Canvas/CosaCanvasDemo/CosaCanvasDemo.ino
@@ -165,226 +165,212 @@ void setup()
 void loop()
 {
   static uint8_t direction = Canvas::PORTRAIT;
-  uint32_t start, ms;
 
   // Test#1: Fill screen
-  start = RTC::micros();
-  tft.set_canvas_color(Canvas::WHITE);
-  tft.fill_screen();
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#1:fill screen: %ul ms", ms);
+  MEASURE("test#1:fill screen:", 1) {
+    tft.set_canvas_color(Canvas::WHITE);
+    tft.fill_screen();
+  }
 
   // Test#2: Use the display as an output stream
-  start = RTC::micros();
-  textbox.set_text_color(Canvas::BLUE);
-  textbox.set_text_scale(1);
-  textbox.set_text_port(2, 2, tft.WIDTH, tft.HEIGHT);
-  console.print(PSTR("CosaCanvasDemo: started"));
-  console.println();
-  console.printf(PSTR("test#1:fill screen %ul ms\n"), ms);
-  console.println();
-  console.printf(PSTR("text_color(%od)\n"), textbox.get_text_color().rgb);
-  console.printf(PSTR("text_scale(%d)\n"), textbox.get_text_scale());
-  uint16_t x, y;
-  textbox.get_cursor(x, y);
-  console.printf(PSTR("cursor(x = %d, y = %d)\n"), x, y);
-  textbox.set_text_color(Canvas::RED);
-  textbox.set_text_scale(2);
-  console.print(PSTR("  Hello\n  World"));
-  console.println();
-  ms = (RTC::micros() - start) / 1000L;
-  textbox.set_text_color(Canvas::BLACK);
-  textbox.set_text_scale(1);
-  console.printf(PSTR("test#2:output stream: %ul ms\n"), ms);
-  INFO("test#2:output stream: %ul ms", ms);
+  MEASURE("test#2:output stream:", 1) {
+    textbox.set_text_color(Canvas::BLUE);
+    textbox.set_text_scale(1);
+    textbox.set_text_port(2, 2, tft.WIDTH, tft.HEIGHT);
+    console.print(PSTR("CosaCanvasDemo: started"));
+    console.println();
+    console.printf(PSTR("text_color(%od)\n"), textbox.get_text_color().rgb);
+    console.printf(PSTR("text_scale(%d)\n"), textbox.get_text_scale());
+    uint16_t x, y;
+    textbox.get_cursor(x, y);
+    console.printf(PSTR("cursor(x = %d, y = %d)\n"), x, y);
+    textbox.set_text_color(Canvas::RED);
+    textbox.set_text_scale(2);
+    console.print(PSTR("  Hello\n  World"));
+    console.println();
+    textbox.set_text_color(Canvas::BLACK);
+    textbox.set_text_scale(1);
+  }
   sleep(2);
 
   // Test#3: Scroll text port
-  start = RTC::micros();
   tft.set_canvas_color(tft.shade(Canvas::WHITE, 50));
   tft.fill_screen();
   tft.set_canvas_color(Canvas::WHITE);
+  MEASURE("test#3:scroll text mode:", 1) {
 #if defined(USE_TFT_ST7735)
-  tft.draw_rect(4, 4, tft.WIDTH-8, tft.HEIGHT-8);
-  textbox.set_text_port(5, 5, tft.WIDTH-10, tft.HEIGHT-10);
+    tft.draw_rect(4, 4, tft.WIDTH-8, tft.HEIGHT-8);
+    textbox.set_text_port(5, 5, tft.WIDTH-10, tft.HEIGHT-10);
 #endif
 #if defined(USE_TFT_ILI9341)
-  tft.draw_rect(tft.WIDTH/4+4, tft.HEIGHT/4+4,
-		tft.WIDTH/2-8, tft.HEIGHT/2-8);
-  textbox.set_text_port(tft.WIDTH/4+5, tft.HEIGHT/4+5,
-			tft.WIDTH/2-10, tft.HEIGHT/2-10);
+    tft.draw_rect(tft.WIDTH/4+4, tft.HEIGHT/4+4,
+		  tft.WIDTH/2-8, tft.HEIGHT/2-8);
+    textbox.set_text_port(tft.WIDTH/4+5, tft.HEIGHT/4+5,
+			  tft.WIDTH/2-10, tft.HEIGHT/2-10);
 #endif
-  console.print('\f');
-  console.print(&tft, 200, IOStream::hex,
-		tft.get_orientation() == Canvas::PORTRAIT ? 3 : 5);
-  ms = (RTC::micros() - start) / 1000L;
-  console.printf(PSTR("%ul ms"), ms);
-  INFO("test#3:scroll text mode: %ul ms", ms);
+    console.print('\f');
+    console.print(&tft, 200, IOStream::hex,
+		  tft.get_orientation() == Canvas::PORTRAIT ? 3 : 5);
+  }
   sleep(2);
 
   // Test#4: Grid with draw pixel
   tft.fill_screen();
-  tft.set_pen_color(Canvas::BLACK);
-  start = RTC::micros();
-  for (uint16_t x = 0; x < tft.WIDTH; x += 2) {
-    for (uint16_t y = 0; y < tft.HEIGHT; y += 2) {
-      tft.draw_pixel(x, y);
+  MEASURE("test#4:draw pixel grid:", 1) {
+    tft.set_pen_color(Canvas::BLACK);
+    for (uint16_t x = 0; x < tft.WIDTH; x += 2) {
+      for (uint16_t y = 0; y < tft.HEIGHT; y += 2) {
+	tft.draw_pixel(x, y);
+      }
     }
+    tft.set_pen_color(Canvas::RED);
+    tft.fill_rect(20, 20, 20, 20);
   }
-  tft.set_pen_color(Canvas::RED);
-  tft.fill_rect(20, 20, 20, 20);
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#4:draw pixel grid: %ul ms", ms);
   sleep(2);
 
   // Test#5: Grid with draw rectangle
-  tft.set_pen_color(Canvas::BLACK);
-  start = RTC::micros();
-  for (uint16_t x = 0; x < tft.WIDTH; x += 20) {
-    for (uint16_t y = 0; y < tft.HEIGHT; y += 20) {
-      tft.draw_rect(x, y, 20, 20);
+  MEASURE("test#5:draw rect grid:", 1) {
+    tft.set_pen_color(Canvas::BLACK);
+    for (uint16_t x = 0; x < tft.WIDTH; x += 20) {
+      for (uint16_t y = 0; y < tft.HEIGHT; y += 20) {
+	tft.draw_rect(x, y, 20, 20);
+      }
     }
   }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#5:draw rect grid: %ul ms", ms);
   sleep(2);
 
   // Test#6: Fill some of the rectangles
-  tft.set_pen_color(Canvas::WHITE);
-  start = RTC::micros();
-  for (uint16_t x = 0; x < tft.WIDTH; x += 20) {
-    for (uint16_t y = x; y < tft.HEIGHT; y += 40) {
-      tft.fill_rect(x + 1, y + 1, 19, 19);
+  MEASURE("test#6:fill rect grid:", 1) {
+    tft.set_pen_color(Canvas::WHITE);
+    for (uint16_t x = 0; x < tft.WIDTH; x += 20) {
+      for (uint16_t y = x; y < tft.HEIGHT; y += 40) {
+	tft.fill_rect(x + 1, y + 1, 19, 19);
+      }
     }
   }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#6:fill rect grid: %ul ms", ms);
   sleep(2);
 
   // Test#7: Fill circles
   tft.fill_screen();
-  tft.set_text_color(Canvas::WHITE);
-  tft.set_text_scale(1);
-  start = RTC::micros();
-  uint16_t color = Canvas::BLUE;
-  for (uint16_t x = 0; x < tft.WIDTH; x += 30) {
-    for (uint16_t y = 0; y < tft.HEIGHT; y += 30) {
-      tft.set_pen_color(tft.shade(color, y * 100/tft.HEIGHT));
-      tft.fill_circle(x, y, 12);
-      tft.set_cursor(x - 2, y - 3);
-      tft.draw_char('A' + (x*(tft.HEIGHT+30))/900 + y/30);
-      tft.set_pen_color(Canvas::BLACK);
-      tft.draw_circle(x, y, 12);
+  MEASURE("test#7:draw circle grid:", 1) {
+    tft.set_text_color(Canvas::WHITE);
+    tft.set_text_scale(1);
+    uint16_t color = Canvas::BLUE;
+    for (uint16_t x = 0; x < tft.WIDTH; x += 30) {
+      for (uint16_t y = 0; y < tft.HEIGHT; y += 30) {
+	tft.set_pen_color(tft.shade(color, y * 100/tft.HEIGHT));
+	tft.fill_circle(x, y, 12);
+	tft.set_cursor(x - 2, y - 3);
+	tft.draw_char('A' + (x*(tft.HEIGHT+30))/900 + y/30);
+	tft.set_pen_color(Canvas::BLACK);
+	tft.draw_circle(x, y, 12);
+      }
+      color <<= 3;
     }
-    color <<= 3;
   }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#7:draw circle grid: %ul ms", ms);
   sleep(2);
 
   // Test#8: Draw lines
   tft.set_canvas_color(tft.shade(Canvas::WHITE, 20));
   tft.fill_screen();
-  start = RTC::micros();
-  tft.set_pen_color(Canvas::RED);
-  for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
-    tft.draw_line(0, 0, x, tft.HEIGHT - 1);
+  MEASURE("test#8:draw lines:", 1) {
+    tft.set_pen_color(Canvas::RED);
+    for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
+      tft.draw_line(0, 0, x, tft.HEIGHT - 1);
+    }
+    tft.set_pen_color(Canvas::GREEN);
+    for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
+      tft.draw_line(0, 0, tft.WIDTH - 1, y);
+    }
+    tft.set_pen_color(Canvas::BLUE);
+    for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
+      tft.draw_line(tft.WIDTH - 1, 0, x, tft.HEIGHT - 1);
+    }
   }
-  tft.set_pen_color(Canvas::GREEN);
-  for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
-    tft.draw_line(0, 0, tft.WIDTH - 1, y);
-  }
-  tft.set_pen_color(Canvas::BLUE);
-  for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
-    tft.draw_line(tft.WIDTH - 1, 0, x, tft.HEIGHT - 1);
-  }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#8:draw lines: %ul ms", ms);
   sleep(2);
 
   // Test#9: Draw more lines
-  start = RTC::micros();
   tft.set_pen_color(Canvas::BLACK);
   tft.fill_screen();
-  tft.set_pen_color(Canvas::YELLOW);
-  for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
-    tft.draw_line(0, tft.HEIGHT - 1, tft.WIDTH - 1, y);
+  MEASURE("test#9:draw more lines:", 1) {
+    tft.set_pen_color(Canvas::YELLOW);
+    for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
+      tft.draw_line(0, tft.HEIGHT - 1, tft.WIDTH - 1, y);
+    }
+    for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
+      tft.draw_line(tft.WIDTH - 1, tft.HEIGHT - 1, x, 0);
+    }
+    for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
+      tft.draw_line(tft.WIDTH - 1, tft.HEIGHT - 1, 0, y);
+    }
   }
-  for (uint16_t x = 0; x < tft.WIDTH; x += 6) {
-    tft.draw_line(tft.WIDTH - 1, tft.HEIGHT - 1, x, 0);
-  }
-  for (uint16_t y = 0; y < tft.HEIGHT; y += 6) {
-    tft.draw_line(tft.WIDTH - 1, tft.HEIGHT - 1, 0, y);
-  }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#9:draw more lines: %ul ms", ms);
   sleep(2);
 
   // Test#10: Display polygons
-  start = RTC::micros();
   tft.set_canvas_color(Canvas::WHITE);
   tft.fill_screen();
-  static const int8_t polygon[] __PROGMEM = {
-    100, 100,
-    -100, 0,
-    50, -50,
-    0, 50,
-    50, 50,
-    -10, -10,
-    0, 0
-  };
-  for (uint16_t x = 10; x < tft.WIDTH - 40; x += 10) {
-    tft.set_pen_color(tft.shade(Canvas::GREEN, 20 + x));
-    tft.set_cursor(10 + x, 50);
-    tft.draw_poly_P(polygon);
+  MEASURE("test#10:polygon:", 1) {
+    static const int8_t polygon[] __PROGMEM = {
+      100, 100,
+      -100, 0,
+      50, -50,
+      0, 50,
+      50, 50,
+      -10, -10,
+      0, 0
+    };
+    for (uint16_t x = 10; x < tft.WIDTH - 40; x += 10) {
+      tft.set_pen_color(tft.shade(Canvas::GREEN, 20 + x));
+      tft.set_cursor(10 + x, 50);
+      tft.draw_poly_P(polygon);
+    }
   }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#10:polygon: %ul ms", ms);
   sleep(2);
 
   // Test#11: Display stroke
-  start = RTC::micros();
   tft.set_canvas_color(Canvas::WHITE);
   tft.fill_screen();
-  static const int8_t stroke[] __PROGMEM = {
-    20, -100,
-    20, 100,
-    -30, -50,
-    20, 0,
-    0, 0
-  };
-  for (uint16_t x = 10; x < tft.WIDTH - 40; x += 10) {
-    tft.set_pen_color(tft.shade(Canvas::RED, 20 + x));
-    tft.set_cursor(x, 120);
-    tft.draw_stroke_P(stroke);
+  MEASURE("test#11:stroke:", 1) {
+    static const int8_t stroke[] __PROGMEM = {
+      20, -100,
+      20, 100,
+      -30, -50,
+      20, 0,
+      0, 0
+    };
+    for (uint16_t x = 10; x < tft.WIDTH - 40; x += 10) {
+      tft.set_pen_color(tft.shade(Canvas::RED, 20 + x));
+      tft.set_cursor(x, 120);
+      tft.draw_stroke_P(stroke);
+    }
   }
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#11:stroke: %ul ms", ms);
   sleep(2);
 
   // Test#12: Display the Arduino Icons
   tft.set_canvas_color(tft.shade(Canvas::WHITE, 10));
   tft.fill_screen();
   tft.set_pen_color(tft.shade(Canvas::CYAN, 80));
-  tft.draw_icon((tft.WIDTH-34)/2, (tft.HEIGHT-32)/2, arduino_icon_34x32);
+  MEASURE("test#12a:draw arduino icon 34x32:", 1) {
+    tft.draw_icon((tft.WIDTH-34)/2, (tft.HEIGHT-32)/2, arduino_icon_34x32);
+  }
   sleep(2);
   tft.fill_screen();
-  tft.draw_icon((tft.WIDTH-64)/2, (tft.HEIGHT-64)/2, arduino_icon_64x64);
+  MEASURE("test#12b:draw arduino icon 64x64:", 1) {
+    tft.draw_icon((tft.WIDTH-64)/2, (tft.HEIGHT-64)/2, arduino_icon_64x64);
+  }
   sleep(2);
   tft.fill_screen();
-  start = RTC::micros();
-  tft.draw_icon((tft.WIDTH-96)/2, (tft.HEIGHT-32)/2, arduino_icon_96x32);
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#12:draw arduino icon: %ul ms", ms);
+  MEASURE("test#12c:draw arduino icon 96x32:", 1) {
+    tft.draw_icon((tft.WIDTH-96)/2, (tft.HEIGHT-32)/2, arduino_icon_96x32);
+  }
   sleep(2);
 
   // Test#13: Grid with draw image
   tft.fill_screen();
-  start = RTC::micros();
-  Grid grid(tft.WIDTH, tft.HEIGHT, Canvas::RED);
-  tft.draw_image(0, 0, &grid);
-  ms = (RTC::micros() - start) / 1000L;
-  INFO("test#13:draw grid image: %ul ms", ms);
+  MEASURE("test#13:draw grid image:", 1) {
+    Grid grid(tft.WIDTH, tft.HEIGHT, Canvas::RED);
+    tft.draw_image(0, 0, &grid);
+  }
   sleep(2);
 
   // Rotate display
