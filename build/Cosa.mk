c8ce2273af308103813b7b5998b62c408c4c6adb
Mikael Patel <mikael.patel@gmail.com>
2015-05-09 18:10:35 +0200

    Support Arduino 1.6.4

--- build/Cosa.mk
+++ build/Cosa.mk
@@ -21,7 +21,7 @@ ifdef GCC_AVR_VER
   ARDUINO_VER = $(GCC_AVR_VER)
 else
   ifndef ARDUINO_VER
-    ARDUINO_VER = 1.6.3
+    ARDUINO_VER = 1.6.4
   endif
   ifndef ARDUINO_DIR
     ARDUINO_DIR = $(HOME)/opt/arduino-$(ARDUINO_VER)
