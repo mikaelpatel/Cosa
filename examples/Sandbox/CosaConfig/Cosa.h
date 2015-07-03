2ba1a05a60fa01b2740a4ce320ff1d32908ad73f
Mikael Patel <mikael.patel@gmail.com>
2015-07-03 18:02:48 +0200

    Fix Arduino IDE build setting to allow local configuration.

---
+++ examples/Sandbox/CosaConfig/Cosa.h
@@ -0,0 +1,57 @@
+/**
+ * @file Cosa.h
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Default for per-application customization of Cosa. This file is
+ * intentionally empty as it is the default customization if the
+ * application does not provide one.
+ *
+ * @section Configuration
+ * To customize Cosa, copy this file into the application directory
+ * and enable define below.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#ifndef COSA_H
+#define COSA_H
+
+/**
+ * Software disable low voltage detect. Powersaving is 23 uA at 5 V.
+ * In file: main.cpp
+ */
+#define COSA_BOD_DISABLE
+
+/**
+ * Event queue size. Default is 16 entries (8 ATTINY).
+ * In file: Cosa/Event.hh
+ */
+#define COSA_EVENT_QUEUE_MAX 8
+
+/**
+ * UART buffer size. Default is 32 characters (16 ATTINY).
+ * In file: Cosa/IOStream/Driver/UART.hh
+ */
+#define COSA_UART_BUFFER_MAX 64
+
+/**
+ * Soft::UART buffer size. Default is 32 characters (16 ATTINY).
+ * In file: Cosa/Soft/UART.hh
+ */
+#define COSA_SOFT_UART_BUFFER_MAX 8
+
+#endif
---
+++ examples/Sandbox/CosaConfig/CosaConfig.ino
@@ -0,0 +1,52 @@
+/**
+ * @file CosaConfig.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Cosa local configuration demonstration and tests.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa.h"
+#include "Cosa/Types.h"
+#include "Cosa/Trace.hh"
+#include "Cosa/IOStream/Driver/UART.hh"
+
+void setup()
+{
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaConfig: started"));
+
+  sleep(1);
+  int available = uart.available();
+  int room = uart.room();
+  TRACE(available);
+  TRACE(room);
+
+#ifdef COSA_BOD_DISABLE
+  trace << PSTR("COSA_BOD_DISABLE defined") << endl;
+#endif
+  TRACE(COSA_EVENT_QUEUE_MAX);
+  TRACE(COSA_UART_BUFFER_MAX);
+  TRACE(COSA_SOFT_UART_BUFFER_MAX);
+}
+
+void loop()
+{
+  ASSERT(true == false);
+}
+
