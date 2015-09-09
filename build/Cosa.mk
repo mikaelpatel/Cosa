e29e8f4be6b2289d3777f789ef3aaa3befe346dd
Mikael Patel <mikael.patel@gmail.com>
2015-09-09 09:58:13 +0200

    Use Arduino IDE/AVR Tools 1.6.5-r5 as default.

--- build/Cosa.mk
+++ build/Cosa.mk
@@ -21,7 +21,7 @@ ifdef GCC_AVR_VER
   ARDUINO_VER = $(GCC_AVR_VER)
 else
   ifndef ARDUINO_VER
-    ARDUINO_VER = 1.6.5
+    ARDUINO_VER = 1.6.5-r5
   endif
   ifndef ARDUINO_DIR
     ARDUINO_DIR = $(HOME)/opt/arduino-$(ARDUINO_VER)
