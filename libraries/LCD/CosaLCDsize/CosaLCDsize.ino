1fe137caff7bef36bfd992d4efb987361974a27c
Mikael Patel <mikael.patel@gmail.com>
2015-09-14 18:53:41 +0200

    Collect LCD size statistics after refactor.

--- libraries/LCD/CosaLCDsize/CosaLCDsize.ino
+++ libraries/LCD/CosaLCDsize/CosaLCDsize.ino
@@ -27,23 +27,30 @@
 
 #include "Cosa/Watchdog.hh"
 
-// AVR Tools: 3.4.3, GCC: 4.3.1
+// GCC: 4.8.1
 // Binary sketch size:  MEGA/STD*/TINY bytes
 // =========================================
-// Baseline:		 954/ 840/ 994 bytes
-// HD44780::Port4b:	3780/3638/3726 bytes
-// HD44780::SR3W:	3692/3544/3628 bytes
-// HD44780::SR3WSPI:	4240/4026/3964 bytes
-// HD44780::SR4W:	3586/3442/3532 bytes
-// HD44780::MJKDZ:	5058/5002/5652 bytes
-// HD44780::GYIICLCD:	5058/5002/5652 bytes
-// HD44780::DFRobot:	5032/4976/5626 bytes
-// HD44780::ERM1602_5:	3866/3732/3812 bytes
-// PCD8544::Serial3W:	4660/4442/4386 bytes
-// PCD8544::SPI3W:	5198/4906/4744 bytes
-// ST7565::Serial3W:	4834/4628/4562 bytes
-// ST7565::SPI3W:	5374/5092/4920 bytes
-// VLCD:		3958/3904/4520 bytes
+// Baseline:		 954/ 596/ 514 bytes
+// HD44780::Port4b:	3984/3780/3674 bytes
+// HD44780::SR3W:	3858/3654/3540 bytes
+// HD44780::SR3WSPI:	3890/3752/3716 bytes
+// HD44780::SR4W:	3708/3504/3398 bytes
+// -----------------------------------------
+// HD44780::MJKDZ:	5358/5200/5542 bytes
+// HD44780::GYIICLCD:	5380/5222/5564 bytes
+// HD44780::DFRobot:	5328/5170/5512 bytes
+// HD44780::Sainsmart:	5328/5170/5512 bytes
+// HD44780::Adafruit:	5378/5220/5834 bytes
+// -----------------------------------------
+// HD44780::ERM1602_5:	3966/3758/3652 bytes
+// =========================================
+// PCD8544::Serial3W:	5314/5110/4760 bytes
+// PCD8544::SPI3W:	5334/5128/4964 bytes
+// =========================================
+// ST7565::Serial3W:	4834/4628/4906 bytes
+// ST7565::SPI3W:	5480/5274/5510 bytes
+// =========================================
+// VLCD:		4394/4236/4844 bytes
 // =========================================
 // The baseline is the Watchdog and Arduino
 // init code. Move comment prefix to compile
@@ -52,15 +59,15 @@
 
 // Select port type to use with the LCD device driver.
 // LCD and communication port
-#include <HD44780.h>
+// #include <HD44780.h>
 
 // HD44780 driver built-in adapters
-HD44780::Port4b port;
+// HD44780::Port4b port;
 // HD44780::SR3W port;
 // HD44780::SR3WSPI port;
 // HD44780::SR4W port;
 
-// I2C expander io port based adapters
+// I2C expander(PCF8574) io port based adapters
 // #include <PCF8574.h>
 // #include <MJKDZ_LCD_Module.h>
 // MJKDZ_LCD_Module port;
@@ -71,6 +78,8 @@ HD44780::Port4b port;
 // DFRobot_IIC_LCD_Module port;
 // #include <SainSmart_LCD2004.h>
 // SainSmart_LCD2004 port;
+
+// I2C expander(MCP23008) io port based adapters
 // #include <MCP23008.h>
 // #include <Adafruit_I2C_LCD_Backpack.h>
 // Adafruit_I2C_LCD_Backpack port;
@@ -82,13 +91,15 @@ HD44780::Port4b port;
 // HD44780 variants; 16X1, 16X2, 16X4, 20X4, default 16X2
 // HD44780 lcd(&port, 20, 4);
 // HD44780 lcd(&port, 16, 4);
-HD44780 lcd(&port);
+// HD44780 lcd(&port);
 
+// #include <Canvas.h>
 // #include <PCD8544.h>
 // LCD::Serial3W port;
 // LCD::SPI3W port;
 // PCD8544 lcd(&port);
 
+// #include <Canvas.h>
 // #include <ST7565.h>
 // LCD::Serial3W port;
 // LCD::SPI3W port;
