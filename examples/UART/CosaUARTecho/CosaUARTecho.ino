054315aa7e5aa77ca74e7c227535fa38e3d11c18
Mikael Patel <mikael.patel@gmail.com>
2015-10-16 02:39:36 +0200

    New UART demo sketch; echo at very high baudrate to verify.

---
+++ examples/UART/CosaUARTecho/CosaUARTecho.ino
@@ -0,0 +1,35 @@
+/**
+ * @file CosaUARTecho.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2013-2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * Test Cosa UART ability to read and write at high speed.
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+#include "Cosa/IOStream/Driver/UART.hh"
+
+void setup()
+{
+  uart.begin(230400);
+}
+
+void loop()
+{
+  while (uart.available())
+    uart.putchar(uart.getchar());
+}
