cc5a3a6c1e7a5ceda65851609491cb4cb3237afb
Daniel Sutcliffe <dans@chairfour.com>
2016-01-05 19:49:32 -0500

    First pass at some boards.txt handling where no Arduino IDE only GCC AVR

--- build/Arduino-Makefile/Arduino.mk
+++ build/Arduino-Makefile/Arduino.mk
@@ -531,13 +531,6 @@ else
 
 endif
 
-# Check if boards.txt should be generated from installed cores. Assume that
-# variant files have been added (symbol links).
-BOARDS_TXT_AVAILABLE := $(call dir_if_exists,$(BOARDS_TXT))
-ifndef BOARDS_TXT_AVAILABLE
-  BOARDS_TXT_CAT := $(shell cat $(ARDUINO_SKETCHBOOK)/hardware/*/boards.txt > $(BOARDS_TXT))
-endif
-
 ########################################################################
 # Miscellaneous
 
