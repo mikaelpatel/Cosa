ca212af71dd4c914a0b659d8143e4d633d4e619d
Mikael Patel <mikael.patel@gmail.com>
2017-06-12 15:04:49 +0200

    Fixed USB core package length.

--- cores/cosa/Cosa/USB/Core.cpp
+++ cores/cosa/Cosa/USB/Core.cpp
@@ -297,7 +297,7 @@ USB_SendSpace(uint8_t ep)
 {
   LockEP lock(ep);
   if (!ReadWriteAllowed()) return (0);
-  return (64 - FifoByteCount());
+  return (64 - 1 - FifoByteCount());
 }
 
 int
@@ -434,10 +434,28 @@ USB_SendControl(uint8_t flags, const void* d, int len)
 int
 USB_RecvControl(void* d, int len)
 {
+  auto length = len;
+  while(length) {
+    // Dont receive more than the USB Control EP has to offer
+    // Use fixed 64 because control EP always have 64 bytes even on 16u2.
+    auto recvLength = length;
+    if(recvLength > 64){
+      recvLength = 64;
+    }
+
+    // Write data to fit to the end (not the beginning) of the array
+    WaitOUT();
+    Recv((uint8_t*)d + len - length, recvLength);
+    ClearOUT();
+    length -= recvLength;
+  }
+  return len;
+#if 0
   WaitOUT();
   Recv((uint8_t*)d,len);
   ClearOUT();
   return (len);
+#endif
 }
 
 int
