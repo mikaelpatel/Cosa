fadbe56d24e9ce64a0b2a418948e63601a8dc267
mikaelpatel <mikael.patel@gmail.com>
2018-11-13 15:44:21 +0000

    Clean build.

--- cores/cosa/Cosa/USB/Core.cpp
+++ cores/cosa/Cosa/USB/Core.cpp
@@ -434,11 +434,11 @@ USB_SendControl(uint8_t flags, const void* d, int len)
 int
 USB_RecvControl(void* d, int len)
 {
-  auto length = len;
-  while(length) {
+  int length = len;
+  while (length) {
     // Dont receive more than the USB Control EP has to offer
     // Use fixed 64 because control EP always have 64 bytes even on 16u2.
-    auto recvLength = length;
+    int recvLength = length;
     if(recvLength > 64){
       recvLength = 64;
     }
